af4630755eb3ddf7923f2a308d25f3bafe6cac36
wzm <46500691@qq.com>
2020-07-15 18:18:40 +0800

    增加图片页

--- miniprogram-foodmap-master/miniprogram/pages/add/add.js
+++ miniprogram-foodmap-master/miniprogram/pages/add/add.js
@@ -2,13 +2,16 @@ const mta = require('../../vendor/mta_analysis.js');
 const app = getApp();
 const db = wx.cloud.database()
 const store = db.collection('store');
+let uploadTask = Array;
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    uploaderList: [],
+    uploaderNum: 0,
+    showUpload: true
   },
 
   /**
@@ -20,10 +23,10 @@ Page({
   chooseLocation: function (event) {
     wx.getSetting({
       success: res => {
-        if (!res.authSetting['scope.userLocation']){
+        if (!res.authSetting['scope.userLocation']) {
           wx.authorize({
             scope: 'scope.userLocation',
-            success:res => {
+            success: res => {
               wx.chooseLocation({
                 success: res => {
                   this.setData({
@@ -36,7 +39,7 @@ Page({
               })
             }
           })
-        }else{
+        } else {
           wx.chooseLocation({
             success: res => {
               this.setData({
@@ -50,11 +53,33 @@ Page({
         }
       }
     })
-    
+
   },
   createItem: function (event) {
     wx.showLoading({
       title: '上传数据中...',
+    })
+    Promise.all(uploadTask).then(result => {
+
+      let urls = [];
+      for (const file of result) {
+        urls.push(file.fileID);
+      }
+      this.setData({
+        images: urls
+      }, res => {
+        wx.hideLoading();
+        wx.showToast({
+          title: '上传图片成功',
+          icon: 'success'
+        })
+      })
+    }).catch(() => {
+      wx.hideLoading()
+      wx.showToast({
+        title: '上传图片错误',
+        icon: 'error'
+      })
     })
     let value = event.detail.value
     store.add({
@@ -75,14 +100,14 @@ Page({
         title: '创建成功！',
         icon: 'success',
         success: res => {
-          wx.navigateBack({
-          })
+          wx.navigateBack({})
         }
       })
     }).catch(error => {
       console.error(error);
     })
   },
+
   uploadImage: function (e) {
     wx.chooseImage({
       count: 9,
@@ -90,7 +115,9 @@ Page({
       sourceType: ['album', 'camera'],
       success: res => {
 
-        wx.showLoading({ title: '上传中' })
+        wx.showLoading({
+          title: '上传中'
+        })
         let tempFilePaths = res.tempFilePaths
         let items = [];
         for (const tempFilePath of tempFilePaths) {
@@ -98,26 +125,13 @@ Page({
             src: tempFilePath
           })
         }
-        const uploadTask = items.map(item => this.uploadPhoto(item.src))
+         uploadTask = items.map(item => this.uploadPhoto(item.src))
 
-        Promise.all(uploadTask).then(result => {
 
-          let urls = [];
-          for (const file of result) {
-            urls.push(file.fileID);
-          }
-          this.setData({
-            images: urls
-          }, res => {
-            wx.hideLoading();
-            wx.showToast({ title: '上传图片成功', icon: 'success' })
-          })
-        }).catch(() => {
-          wx.hideLoading()
-          wx.showToast({ title: '上传图片错误', icon: 'error' })
+
+        this.setData({
+          tempPhoto: items
         })
-
-        this.setData({ tempPhoto: items })
       }
     })
   },
@@ -125,6 +139,69 @@ Page({
     return wx.cloud.uploadFile({
       cloudPath: `${Date.now()}-${Math.floor(Math.random(0, 1) * 10000000)}.png`,
       filePath
+    })
+  },
+  // 删除图片
+  clearImg: function (e) {
+    var nowList = []; //新数据
+    var uploaderList = this.data.uploaderList; //原数据
+
+    for (let i = 0; i < uploaderList.length; i++) {
+      if (i == e.currentTarget.dataset.index) {
+        continue;
+      } else {
+        nowList.push(uploaderList[i])
+      }
+    }
+    this.setData({
+      uploaderNum: this.data.uploaderNum - 1,
+      uploaderList: nowList,
+      showUpload: true
+    })
+  },
+  //展示图片
+  showImg: function (e) {
+    var that = this;
+    wx.previewImage({
+      urls: that.data.uploaderList,
+      current: that.data.uploaderList[e.currentTarget.dataset.index]
+    })
+  },
+  upload: function (e) {
+    var that = this;
+    wx.chooseImage({
+      count: 9 - that.data.uploaderNum, // 默认9
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        console.log(res)
+        let tempFilePaths = res.tempFilePaths
+
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+
+        let uploaderList = that.data.uploaderList.concat(tempFilePaths);
+        if (uploaderList.length == 9) {
+          that.setData({
+            showUpload: false
+          })
+        }
+        that.setData({
+          uploaderList: uploaderList,
+          uploaderNum: uploaderList.length,
+        })
+        let items = [];
+        for (const tempFilePath of tempFilePaths) {
+          items.push({
+            src: tempFilePath
+          })
+        }
+        console.log(item.src);
+
+         uploadTask = items.map(item => this.uploadPhoto(item.src))
+        this.setData({
+          tempPhoto: items
+        })
+      }
     })
   }
 })
